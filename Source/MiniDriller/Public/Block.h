f6553877890224a3b422ea1ebd0783dcc836e26e
YangJiseop <vastu54321@gmail.com>
2026-05-11 10:28:26 +0900

    feat:(block) ABlock 부모 클래스 뼈대 생성

---
+++ Source/MiniDriller/Public/Block.h
@@ -0,0 +1,25 @@
+﻿// 채굴 대상 및 상호작용 오브젝트의 최상위 부모 클래스
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Block.generated.h"
+
+UCLASS()
+class MINIDRILLER_API ABlock : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABlock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// 플레이어와 충돌 및 상호작용 시 실행되는 함수
+	virtual void OnInteracted(class ADrillerCharacter* Player);
+};
